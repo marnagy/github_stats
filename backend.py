3725fe1e29efd79bcfd081a0d71af4cc53a2e199
Marek Nagy <marek.nagy123456@gmail.com>
2025-01-13 22:55:08 +0100

    Added simple backend

--- backend.py
+++ backend.py
@@ -1,7 +1,31 @@
+import os
+from collections import defaultdict
+from datetime import datetime, timedelta
+import json
+from dataclasses import dataclass
+
 from flask import Flask
+
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+REPOSITORIES = os.getenv("REPOSITORIES").split('|')
+MAX_REPOS = int(os.getenv("MAX_REPOSITORIES_AMOUNT", 5)) 
+
+@dataclass(frozen=True, eq=True, kw_only=True)
+class OwnerRepoPair:
+    owner: str
+    repo: str
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.get("/test")
+def test_endpoint():
+    return {"server": "running"}
+
+@app.get("/<owner>/<repo>")
+def get_stats(owner: str, repo: str):
+    return f"{owner} ### {repo}"
+
+# DEV
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)
